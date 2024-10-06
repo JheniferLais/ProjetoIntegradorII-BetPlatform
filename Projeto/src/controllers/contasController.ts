7fa77532378d35f5ea6c126256277cb345bc1436
LJhenifer <jheniferlais3@gmail.com>
2024-10-06 19:42:38 -0300

    refactor: melhor legibilidade de codigo e feat: adiciona o token de sessão

--- Projeto/src/controllers/contasController.ts
+++ Projeto/src/controllers/contasController.ts
@@ -1,7 +1,9 @@
 import { Request, RequestHandler, Response } from 'express';
 import { BDcontas, Conta } from '../models/usuarioModel';
 import { salvarNoBanco } from "../utils/dataBaseUtils";
+import jwt from 'jsonwebtoken';
 //
+
 export namespace contasHandler {
 
     // Função para verificar se o usuario fez aniversario
@@ -27,45 +29,46 @@ export namespace contasHandler {
         const nascimento = req.get('nascimento');
 
         // Verifica se todos os campos foram preenchidos
-        if (nome && email && senha && nascimento) {
-            if (!BDcontas.find(u => u.email === email)) {
-                // Verifica a maioridade do usuario
-                const idade: number = verificaIdade(nascimento);
-
-                if (idade >= 18) {
-
-                    // Cria uma 'novaConta' do tipo 'userAccount' com as informações recebidas
-                    const novaConta: Conta = {
-                        idUsuario: BDcontas.length,
-                        nome: nome,
-                        email: email,
-                        senha: senha,
-                        nascimento: nascimento,
-                    };
-
-                    // Insere no Banco e retorna um id
-                    const id: number = salvarNoBanco(novaConta, BDcontas);
-
-                    const response = {
-                        id: `${id}`,
-                        nome: `${nome}`,
-                        email: `${email}`,
-                        nascimento: `${nascimento}`,
-                    };
-                    res.statusCode = 200;
-                    res.send(response);
-                } else {
-                    //res.statusCode = ?;
-                    res.send(`O usuario deve ser maior de idade!`);
-                }
-            } else {
-                //res.statusCode = ?;
-                res.send(`Usuario já cadastrado!`);
-            }
-        } else {
+        if (!nome || !email || !senha || !nascimento) {
             res.statusCode = 400;
             res.send(`Preencha todos os campos!`);
+            return;
         }
+
+        // Verifica se o email já foi cadastrado
+        if (BDcontas.find(u => u.email === email)) {
+            res.statusCode = 409
+            res.send(`Usuário já cadastrado!`);
+            return;
+        }
+
+        // Verifica a maioridade do usuario
+        const idade: number = verificaIdade(nascimento);
+        if(idade < 18){
+            //res.statusCode = ?;
+            res.send(`O usuario deve ser maior de idade!`);
+        }
+
+        // Cria uma 'novaConta' do tipo 'userAccount' com as informações recebidas
+        const novaConta: Conta = {
+            idUsuario: BDcontas.length,
+            nome: nome,
+            email: email,
+            senha: senha,
+            nascimento: nascimento,
+        };
+
+        // Insere no Banco e retorna um id
+        const id: number = salvarNoBanco(novaConta, BDcontas);
+
+        const response = {
+            id: `${id}`,
+            nome: `${nome}`,
+            email: `${email}`,
+            nascimento: `${nascimento}`,
+        };
+        res.statusCode = 200;
+        res.send(response);
     };
 
     // 'Função' para login
@@ -74,25 +77,35 @@ export namespace contasHandler {
         const senha = req.get('senha');
 
         // Verifica se todos os campos foram preenchidos
-        if (email && senha) {
-
-            // Verifica se o usuario esta no array 'accounts' ou seja cadastrado
-            const user = BDcontas.find(u => u.email === email && u.senha === senha);
-
-            if (user) {
-                const response = {
-                    status: `login efetuado com sucesso!`,
-                    message: `Bem-vindo! ${user.nome}`,
-                };
-                res.statusCode = 200;
-                res.send(response);
-            } else {
-                //res.statusCode = ?;
-                res.send(`Login ou senha inválidos!`);
-            }
-        } else {
+        if (!email || !senha) {
             res.statusCode = 400;
             res.send(`Preencha todos os campos!`);
+            return;
         }
+
+        // Verifica se o usuario esta no array 'accounts' ou seja cadastrado
+        const user = BDcontas.find(u => u.email === email && u.senha === senha);
+        if (!user) {
+            res.statusCode = 401;
+            res.send(`Login ou senha inválidos!`);
+            return;
+        }
+
+        const JWT_SECRET = 'segredo_super_secreto';
+        const token: string = jwt.sign(
+            { idUsuario: user.idUsuario,
+                email: user.email },
+            JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+        //localStorage.setItem('token', token);
+
+        const response = {
+            status: `login efetuado com sucesso!`,
+            message: `Bem-vindo! ${user.nome}`,
+            token: token,
+        };
+        res.statusCode = 200;
+        res.send(response);
     };
 }
