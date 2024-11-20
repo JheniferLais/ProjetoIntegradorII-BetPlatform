a99177cac463004910b67eb4d28ef259dc9244cd
Jhenifer Laís <jheniferlais3@gmail.com>
2024-11-20 14:44:28 -0300

    feat: add 'authorization' to routes that use token

--- Projeto/src/utils/TokenUtils.ts
+++ Projeto/src/utils/TokenUtils.ts
@@ -5,9 +5,9 @@ import { dataBaseutils } from './DataBaseutils';
 export namespace tokenUtils {
 
     //Função para encontrar o token do usuario baseado no id
-    export async function findToken(idUsuario: number): Promise<Conta[][]> {
+    export async function findToken(idUsuario: number, tokenAuth: string): Promise<Conta[][]> {
         const connection = await dataBaseutils.ConnectionDB();
-        const result = await connection.execute("SELECT token FROM usuarios WHERE id_usuario = :idUsuario", [idUsuario]);
+        const result = await connection.execute("SELECT token FROM usuarios WHERE id_usuario = :idUsuario and TOKEN = :tokenAuth", [idUsuario, tokenAuth]);
         await connection.close();
         return result.rows as Conta[][];
     }
@@ -15,15 +15,16 @@ export namespace tokenUtils {
     //Função para verificar se o usuario existe baseado no token
     export const checkToken = async (req: Request, res: Response, next: NextFunction): Promise<any>=> {
         const idUsuario = parseInt(req.params.id); // ID do usuario passado como parâmetro na URL
+        const tokenAuth= req.get('authorization');
 
         // Valida se todos os campos foram preenchidos
-        if(!idUsuario) {
+        if(!idUsuario || !tokenAuth) {
             res.status(400).send('Campos obrigatórios estão faltando!');
             return;
         }
 
         // Valida o token do usuario
-        const token: Conta[][] = await findToken(idUsuario);
+        const token: Conta[][] = await findToken(idUsuario, tokenAuth);
         if (!token || token.length === 0) {
             res.status(403).send('Acesso negado. Token inválido.');
             return;
